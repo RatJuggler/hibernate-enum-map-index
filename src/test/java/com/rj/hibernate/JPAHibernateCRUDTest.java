4f0f44eeba02a1682d475227c5a285c77abc0e1d
John Chase <ratteal@gmail.com>
2018-07-14 16:51:43 +0100

    Test multiple entities with different enum sizes.

--- src/test/java/com/rj/hibernate/JPAHibernateCRUDTest.java
+++ src/test/java/com/rj/hibernate/JPAHibernateCRUDTest.java
@@ -12,7 +12,7 @@ public class JPAHibernateCRUDTest extends JPAHibernateTest {
   }
 
   @Test
-  public void testInitialisedMonster() {
+  public void testMonsterEntity1() {
     Monster monster = em.find(Monster.class, 1);
     assertNotNull(monster);
     assertEquals("Godzilla", monster.getName());
@@ -20,8 +20,24 @@ public class JPAHibernateCRUDTest extends JPAHibernateTest {
   }
 
   @Test
+  public void testMonsterEntity2() {
+    Monster monster = em.find(Monster.class, 2);
+    assertNotNull(monster);
+    assertEquals("Giant Badger", monster.getName());
+    assertEquals(Size.MEDIUM, monster.getSize());
+  }
+
+  @Test
+  public void testMonsterEntity3() {
+    Monster monster = em.find(Monster.class, 3);
+    assertNotNull(monster);
+    assertEquals("Gremlin", monster.getName());
+    assertEquals(Size.SMALL, monster.getSize());
+  }
+
+  @Test
   public void testGetAll() {
-    assertEquals(1, getAllMonsters().size());
+    assertEquals(3, getAllMonsters().size());
   }
 
   @Test
@@ -33,18 +49,18 @@ public class JPAHibernateCRUDTest extends JPAHibernateTest {
     List<Monster> monsters = getAllMonsters();
 
     assertNotNull(monsters);
-    assertEquals(2, monsters.size());
+    assertEquals(4, monsters.size());
   }
 
   @Test
-  public void testDelete_success() {
+  public void testDelete() {
     Monster monster = em.find(Monster.class, 1);
 
     em.getTransaction().begin();
     em.remove(monster);
     em.getTransaction().commit();
 
-    assertEquals(0, getAllMonsters().size());
+    assertEquals(2, getAllMonsters().size());
   }
 
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,4 @@
 DELETE FROM Monster;
 INSERT INTO Monster(id, name, size) VALUES (1, 'Godzilla', 0);
+INSERT INTO Monster(id, name, size) VALUES (2, 'Giant Badger', 1);
+INSERT INTO Monster(id, name, size) VALUES (3, 'Gremlin', 2);
