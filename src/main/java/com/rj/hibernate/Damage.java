9564dca6adfff22fe35109d1fe1fbcd8a565199d
John Chase <ratteal@gmail.com>
2018-07-14 19:09:57 +0100

    Create the new Damage entity and join it in the existing Monster.

---
+++ src/main/java/com/rj/hibernate/Damage.java
@@ -0,0 +1,67 @@
+package com.rj.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Damage {
+
+  @Id
+  private Integer id;
+
+  @ManyToOne
+  @JoinColumn(name = "MONSTERID", nullable = false)
+  private Monster monster;
+
+  private Attack attack;
+
+  private Integer multiplier;
+
+  public Damage() {}
+
+  public Damage(Integer id, Attack attack, Integer multiplier) {
+    this.id = id;
+    this.attack = attack;
+    this.multiplier = multiplier;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Monster getMonster() {
+    return monster;
+  }
+
+  public void setMonster(Monster monster) {
+    this.monster = monster;
+  }
+
+  public Attack getAttack() {
+    return attack;
+  }
+
+  public void setAttack(Attack attack) {
+    this.attack = attack;
+  }
+
+  public Integer getMultiplier() {
+    return multiplier;
+  }
+
+  public void setMultiplier(Integer multiplier) {
+    this.multiplier = multiplier;
+  }
+
+  @Override
+  public String toString() {
+    return "Damage{" + "id=" + id + ", attack=" + attack + ", multiplier=" + multiplier + '}';
+  }
+
+}
--- src/main/java/com/rj/hibernate/Monster.java
+++ src/main/java/com/rj/hibernate/Monster.java
@@ -1,16 +1,21 @@
 package com.rj.hibernate;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.MapKey;
+import javax.persistence.MapKeyEnumerated;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 @Entity
-@NamedQueries(value = {
-  @NamedQuery(name = "Monster.getAll", query = "SELECT m FROM Monster m")
-})
+@NamedQueries(value = { @NamedQuery(name = "Monster.getAll", query = "SELECT m FROM Monster m") })
 public class Monster {
 
   @Id
@@ -20,8 +25,12 @@ public class Monster {
   @Enumerated(EnumType.ORDINAL)
   private Size size;
 
-  public Monster() {
-  }
+  @OneToMany(mappedBy = "monster", cascade = CascadeType.ALL, orphanRemoval = true)
+  @MapKey(name = "attack")
+  @MapKeyEnumerated
+  private Map<Attack, Damage> attacks = new HashMap<>();
+
+  public Monster() {}
 
   public Monster(Integer id, String name, Size size) {
     this.id = id;
@@ -53,9 +62,22 @@ public class Monster {
     this.size = size;
   }
 
+  public Map<Attack, Damage> getAttacks() {
+    return attacks;
+  }
+
+  public void addAttack(Damage damage) {
+    damage.setMonster(this);
+    attacks.put(damage.getAttack(), damage);
+  }
+
   @Override
   public String toString() {
-    return "Monster{" + "id=" + id + ", name='" + name + "\', size=" + size + '}';
+    String monster = "Monster{" + "id=" + id + ", name='" + name + "\', size=" + size + '}';
+    for (Damage damage : attacks.values()) {
+      monster += "\n- " + damage;
+    }
+    return monster;
   }
 
 }
