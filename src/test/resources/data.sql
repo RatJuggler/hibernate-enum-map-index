9349f0799fd1cff76bf437eb58cfb6b82baa9e0d
John Chase <ratteal@gmail.com>
2018-07-14 19:11:51 +0100

    Add data we can work with in tests.

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,10 @@
+DELETE FROM Damage;
 DELETE FROM Monster;
 INSERT INTO Monster(id, name, size) VALUES (1, 'Godzilla', 0);
 INSERT INTO Monster(id, name, size) VALUES (2, 'Giant Badger', 1);
 INSERT INTO Monster(id, name, size) VALUES (3, 'Gremlin', 2);
+INSERT INTO Damage(id, monsterId, attack, multiplier) VALUES (1, 1, 4, 1000);
+INSERT INTO Damage(id, monsterId, attack, multiplier) VALUES (2, 1, 2, 100);
+INSERT INTO Damage(id, monsterId, attack, multiplier) VALUES (3, 2, 0, 1);
+INSERT INTO Damage(id, monsterId, attack, multiplier) VALUES (4, 2, 1, 2);
+INSERT INTO Damage(id, monsterId, attack, multiplier) VALUES (5, 3, 3, 5);
