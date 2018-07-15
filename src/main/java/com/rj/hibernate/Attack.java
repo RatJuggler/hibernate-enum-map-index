cffe9836c20c188f8d4cee7c3f9ee1827d6a0153
John Chase <ratteal@gmail.com>
2018-07-15 11:47:34 +0100

    Some comments on how new enums would break the tests.

--- src/main/java/com/rj/hibernate/Attack.java
+++ src/main/java/com/rj/hibernate/Attack.java
@@ -2,6 +2,7 @@ package com.rj.hibernate;
 
 public enum Attack {
 
+//ROAR,  Adding this would break the tests because it changes the Ordinals.
   BITE,
   CLAW,
   STOMP,
--- src/main/java/com/rj/hibernate/Size.java
+++ src/main/java/com/rj/hibernate/Size.java
@@ -2,6 +2,7 @@ package com.rj.hibernate;
 
 public enum Size {
 
+//  ENORMOUS,  Adding this would break the tests because it changes the Ordinals.
   LARGE, 
   MEDIUM, 
   SMALL;
